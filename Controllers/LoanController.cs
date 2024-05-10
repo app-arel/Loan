86c0024f112f9b544f5097f5fb58247ef0c6020a
app-arel <100846852+app-arel@users.noreply.github.com>
2024-05-10 18:44:05 +0100

    Add files via upload
    LoanApp

---
+++ Controllers/LoanController.cs
@@ -0,0 +1,45 @@
+﻿using Loan.Data;
+using Loan.Models.Entities;
+using Loan.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loan.Controllers
+{
+    public class LoanController : Controller
+    {
+        private readonly LoanContext context;
+
+        public LoanController(LoanContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Loan()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Loan(LoanViewModel vModel)
+        {
+            var interest = 1.10;
+            var amountToPay = vModel.Amount * (1.10 * vModel.Tenure);
+            var monthlyPayment = amountToPay / 12;
+            var loan = new LoanDetails
+            {
+                Amount = vModel.Amount,
+                Tenure = vModel.Tenure,
+                AmountToPay = amountToPay,
+                Interest = interest,
+                MonthlyPayment = monthlyPayment
+            };
+            context.loans.Add(loan);
+            context.SaveChanges();
+            return View("Loan");
+        }
+    }
+}
